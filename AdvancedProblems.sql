203ce7cc82de4a53522f0c84b70fd951921880be
cimentadaj <cimentadaj@gmail.com>
2018-04-22 11:35:03 +0200

    Finished ex 49 - 7 to go

--- AdvancedProblems.sql
+++ AdvancedProblems.sql
@@ -77,10 +77,171 @@ ORDER BY Quantity;
 SELECT od.OrderID, ProductID, UnitPrice, Quantity, Discount
 FROM OrderDetails AS od
 JOIN (
-	SELECT OrderID
+	SELECT DISTINCT OrderID
 	FROM OrderDetails
 	WHERE Quantity >= 60
 	GROUP BY OrderID, Quantity
 	HAVING COUNT(*) > 1
 ) as ode
 ON od.OrderID = ode.OrderID;
+
+/* Exercise 41 */
+
+SELECT OrderID,
+		CAST(OrderDate AS DATE) AS OrderDate,
+		CAST(RequiredDate AS DATE) AS RequiredDate,
+        CAST(ShippedDate AS DATE) AS ShippedDate
+FROM Orders
+WHERE ShippedDate > RequiredDate;
+
+/* Exercise 42 */
+
+SELECT o.EmployeeID, e.LastName, COUNT(*) AS TotalLateOrders
+FROM Employees AS e
+JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+WHERE CAST(o.ShippedDate AS DATE) >= CAST(o.RequiredDate AS DATE)
+GROUP BY o.EmployeeID, e.LastName
+ORDER BY COUNT(*) DESC;
+
+/* Exercise 43 */
+
+SELECT e.EmployeeID, e.LastName, COUNT(*) AS TotalOrders, late.TotalLateOrders
+FROM Employees AS e
+JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+JOIN (
+	SELECT o.EmployeeID, e.LastName, COUNT(*) AS TotalLateOrders
+	FROM Employees AS e
+	JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+	WHERE CAST(o.ShippedDate AS DATE) >= CAST(o.RequiredDate AS DATE)
+	GROUP BY o.EmployeeID, e.LastName
+	ORDER BY COUNT(*) DESC
+) AS late ON e.EmployeeID = late.EmployeeID
+GROUP BY e.EmployeeID, e.LastName;
+
+/* Exercise 44 */
+
+SELECT e.EmployeeID, e.LastName, COUNT(*) AS TotalOrders, late.TotalLateOrders
+FROM Employees AS e
+JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+LEFT JOIN (
+	SELECT o.EmployeeID, e.LastName, COUNT(*) AS TotalLateOrders
+	FROM Employees AS e
+	JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+	WHERE CAST(o.ShippedDate AS DATE) >= CAST(o.RequiredDate AS DATE)
+	GROUP BY o.EmployeeID, e.LastName
+	ORDER BY COUNT(*) DESC
+) AS late ON e.EmployeeID = late.EmployeeID
+GROUP BY e.EmployeeID, e.LastName;
+
+/* Exercise 45 */
+
+SELECT e.EmployeeID,
+	   e.LastName,
+       COUNT(*) AS TotalOrders,
+       CASE WHEN late.TotalLateOrders IS NULL THEN 0 ELSE late.TotalLateOrders END AS TotalLateOrders
+FROM Employees AS e
+JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+LEFT JOIN (
+	SELECT o.EmployeeID, e.LastName, COUNT(*) AS TotalLateOrders
+	FROM Employees AS e
+	JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+	WHERE CAST(o.ShippedDate AS DATE) >= CAST(o.RequiredDate AS DATE)
+	GROUP BY o.EmployeeID, e.LastName
+	ORDER BY COUNT(*) DESC
+) AS late ON e.EmployeeID = late.EmployeeID
+GROUP BY e.EmployeeID, e.LastName;
+
+/* Exercise 46 */
+
+SELECT e.EmployeeID,
+	   e.LastName,
+       COUNT(*) AS TotalOrders,
+       CASE WHEN late.TotalLateOrders IS NULL
+       THEN 0
+       ELSE late.TotalLateOrders
+       END AS TotalLateOrders,
+       (CASE WHEN late.TotalLateOrders IS NULL
+       THEN 0
+       ELSE late.TotalLateOrders
+       END) / COUNT(*) AS 'Percent Late Orders'
+FROM Employees AS e
+JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+LEFT JOIN (
+	SELECT o.EmployeeID, e.LastName, COUNT(*) AS TotalLateOrders
+	FROM Employees AS e
+	JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+	WHERE CAST(o.ShippedDate AS DATE) >= CAST(o.RequiredDate AS DATE)
+	GROUP BY o.EmployeeID, e.LastName
+	ORDER BY COUNT(*) DESC
+) AS late ON e.EmployeeID = late.EmployeeID
+GROUP BY e.EmployeeID, e.LastName;
+
+
+/* Exercise 47 */
+
+SELECT e.EmployeeID,
+	   e.LastName,
+       COUNT(*) AS TotalOrders,
+       CASE WHEN late.TotalLateOrders IS NULL
+       THEN 0
+       ELSE late.TotalLateOrders
+       END AS TotalLateOrders,
+       CAST((CASE WHEN late.TotalLateOrders IS NULL
+       THEN 0
+       ELSE late.TotalLateOrders
+       END) / COUNT(*) AS DECIMAL(8,2)) AS 'Percent Late Orders'
+FROM Employees AS e
+JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+LEFT JOIN (
+	SELECT o.EmployeeID, e.LastName, COUNT(*) AS TotalLateOrders
+	FROM Employees AS e
+	JOIN Orders AS o ON o.EmployeeID = e.EmployeeID
+	WHERE CAST(o.ShippedDate AS DATE) >= CAST(o.RequiredDate AS DATE)
+	GROUP BY o.EmployeeID, e.LastName
+	ORDER BY COUNT(*) DESC
+) AS late ON e.EmployeeID = late.EmployeeID
+GROUP BY e.EmployeeID, e.LastName;
+
+/* Exercise 48 */
+
+SELECT *,
+	   CASE WHEN
+       totorder BETWEEN 0 AND 1000 THEN 'Low' WHEN
+       totorder BETWEEN 1000 AND 5000 THEN 'Medium' WHEN
+       totorder BETWEEN 5000 AND 10000 THEN 'High' WHEN
+       totorder >= 10000 THEN 'Very high' 
+       END AS customergroup
+FROM (
+	SELECT o.CustomerID,
+		   c.CompanyName,
+		   SUM(od.UnitPrice * od.Quantity) AS totorder
+	FROM Orders AS o
+	JOIN OrderDetails AS od ON o.OrderID = od.OrderID
+	JOIN Customers AS c ON o.CustomerID = c.CustomerID
+	WHERE o.OrderDate BETWEEN '20160101' AND '20170101'
+	GROUP BY o.CustomerID, c.CompanyName
+	ORDER BY o.CustomerID, c.CompanyName
+) AS mt;
+
+
+/* Exercise 49 */
+
+SELECT *,
+	   CASE WHEN
+       totorder BETWEEN 0 AND 1000 THEN 'Low' WHEN
+       totorder BETWEEN 1000 AND 5000 THEN 'Medium' WHEN
+       totorder BETWEEN 5000 AND 10000 THEN 'High' WHEN
+       totorder >= 10000 THEN 'Very high' 
+       END AS customergroup
+FROM (
+	SELECT o.CustomerID,
+		   c.CompanyName,
+		   SUM(od.UnitPrice * od.Quantity) AS totorder
+	FROM Orders AS o
+	JOIN OrderDetails AS od ON o.OrderID = od.OrderID
+	JOIN Customers AS c ON o.CustomerID = c.CustomerID
+	WHERE o.OrderDate BETWEEN '20160101' AND '20170101'
+	GROUP BY o.CustomerID, c.CompanyName
+	ORDER BY o.CustomerID, c.CompanyName
+) AS mt
+WHERE mt.CustomerID = 'MAISD';
